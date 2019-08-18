d157e46664e2fe3a5903729fb7afe90e4bdb3cd3
Eyal Kalderon <ebkalderon@gmail.com>
2019-08-18 21:43:44 +0800

    Document current roadblocks with Partial

--- nix-parser/src/parser/expr/atomic.rs
+++ nix-parser/src/parser/expr/atomic.rs
@@ -1,10 +1,15 @@
+use std::iter::FromIterator;
+
 use nom::character::complete::char;
-use nom::combinator::{cut, map};
+use nom::combinator::{cut, map, map_parser, recognize};
+use nom::multi::{many0, many_till};
 use nom::sequence::{delimited, pair, preceded, terminated};
 
 use super::{bind, expr};
 use crate::ast::{ExprParen, ExprSet};
-use crate::parser::partial::{many0_partial, map_partial, verify_full, Partial};
+use crate::parser::partial::{
+    many0_partial, map_partial, partial, partial_until, verify_full, Partial,
+};
 use crate::parser::{tokens, IResult, Span};
 use crate::ToByteSpan;
 
@@ -15,7 +20,29 @@ pub fn paren(input: Span) -> IResult<ExprParen> {
 }
 
 pub fn set(input: Span) -> IResult<Partial<ExprSet>> {
-    let binds = many0_partial(";", preceded(tokens::space_until_final_comment, bind::bind));
-    let set = delimited(char('{'), terminated(binds, tokens::space), cut(char('}')));
+    // 1. Works correctly in all cases, but isn't partial.
+    //
+    // let binds = map(
+    //     many0(preceded(tokens::space_until_final_comment, bind::bind)),
+    //     Partial::from,
+    // );
+    // let set = delimited(char('{'), terminated(binds, tokens::space), cut(char('}')));
+
+    // 2. Is partial, but only supports empty sets.
+    //
+    let binds = map(
+        many_till(
+            preceded(tokens::space_until_final_comment, partial(bind::bind)),
+            pair(tokens::space, char('}')),
+        ),
+        |(p, _)| Partial::from_iter(p),
+    );
+    let set = preceded(pair(char('{'), tokens::space_until_final_comment), binds);
+
+    // 3. Original, is partial, and does not support nested sets of any kind.
+    //
+    // let binds = many0_partial(";", preceded(tokens::space_until_final_comment, bind::bind));
+    // let set = delimited(char('{'), terminated(binds, tokens::space), cut(char('}')));
+
     map_partial(set, |binds| ExprSet::new(binds, input.to_byte_span()))(input)
 }

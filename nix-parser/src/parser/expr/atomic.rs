4454dbea311df18dd237dbb062f984df98cb40d2
Eyal Kalderon <ebkalderon@gmail.com>
2019-08-20 21:05:10 +0800

    Update set() to use many_till(), add invalid error case to paren()

--- nix-parser/src/parser/expr/atomic.rs
+++ nix-parser/src/parser/expr/atomic.rs
@@ -1,47 +1,38 @@
 use std::iter::FromIterator;
 
+use nom::branch::alt;
+use nom::bytes::complete::take;
 use nom::character::complete::char;
 use nom::combinator::{cut, map, map_parser, recognize};
+use nom::error::{ErrorKind, VerboseError, VerboseErrorKind};
 use nom::multi::{many0, many_till};
 use nom::sequence::{delimited, pair, preceded, terminated};
 
 use super::{bind, expr};
 use crate::ast::{ExprParen, ExprSet};
-use crate::parser::partial::{
-    many0_partial, map_partial, partial, partial_until, verify_full, Partial,
-};
+use crate::parser::partial::{map_partial, partial_or, Partial};
 use crate::parser::{tokens, IResult, Span};
 use crate::ToByteSpan;
 
 pub fn paren(input: Span) -> IResult<Partial<ExprParen>> {
-    let paren = delimited(pair(char('('), tokens::space), expr, cut(char(')')));
-    map_partial(paren, |e| ExprParen::new(Box::new(e), input.to_byte_span()))(input)
+    let paren = delimited(pair(char('('), tokens::space), expr, char(')'));
+    let valid = map_partial(paren, |e| ExprParen::new(Box::new(e), input.to_byte_span()));
+
+    let invalid = map(preceded(pair(char('('), tokens::space), expr), |mut expr| {
+        let err = VerboseError {
+            errors: vec![(input, VerboseErrorKind::Char(')'))],
+        };
+        expr.extend_errors(err);
+        expr.map(|expr| ExprParen::new(Box::new(expr), input.to_byte_span()))
+    });
+
+    alt((valid, invalid))(input)
 }
 
 pub fn set(input: Span) -> IResult<Partial<ExprSet>> {
-    // 1. Works correctly in all cases, but isn't partial.
-    //
-    // let binds = map(
-    //     many0(preceded(tokens::space_until_final_comment, bind::bind)),
-    //     Partial::from,
-    // );
-    // let set = delimited(char('{'), terminated(binds, tokens::space), cut(char('}')));
-
-    // 2. Is partial, but only supports empty sets.
-    //
-    let binds = map(
-        many_till(
-            preceded(tokens::space_until_final_comment, partial(bind::bind)),
-            pair(tokens::space, char('}')),
-        ),
-        |(p, _)| Partial::from_iter(p),
-    );
+    let bind = preceded(tokens::space_until_final_comment, bind::bind);
+    let close_brace = pair(tokens::space, char('}'));
+    let binds = map(many_till(bind, close_brace), |(p, _)| Partial::from_iter(p));
     let set = preceded(pair(char('{'), tokens::space_until_final_comment), binds);
-
-    // 3. Original, is partial, and does not support nested sets of any kind.
-    //
-    // let binds = many0_partial(";", preceded(tokens::space_until_final_comment, bind::bind));
-    // let set = delimited(char('{'), terminated(binds, tokens::space), cut(char('}')));
-
     map_partial(set, |binds| ExprSet::new(binds, input.to_byte_span()))(input)
 }

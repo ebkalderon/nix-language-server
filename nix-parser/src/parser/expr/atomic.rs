d1ea7955b5136be8344f8a875b0d1bf81a7c006e
Eyal Kalderon <ebkalderon@gmail.com>
2019-08-21 00:36:40 +0800

    Fix spans for set() and paren() using map_partial_spanned()

--- nix-parser/src/parser/expr/atomic.rs
+++ nix-parser/src/parser/expr/atomic.rs
@@ -10,19 +10,18 @@ use nom::sequence::{delimited, pair, preceded, terminated};
 
 use super::{bind, expr};
 use crate::ast::{ExprParen, ExprSet};
-use crate::parser::partial::{expect_terminated, map_partial, Partial};
+use crate::parser::partial::{expect_terminated, map_partial_spanned, Partial};
 use crate::parser::{tokens, IResult, Span};
 use crate::ToByteSpan;
 
 pub fn paren(input: Span) -> IResult<Partial<ExprParen>> {
     let paren = expect_terminated(preceded(pair(char('('), tokens::space), expr), char(')'));
-    map_partial(paren, |e| ExprParen::new(Box::new(e), input.to_byte_span()))(input)
+    map_partial_spanned(input, paren, |span, e| ExprParen::new(Box::new(e), span))(input)
 }
 
 pub fn set(input: Span) -> IResult<Partial<ExprSet>> {
     let bind = preceded(tokens::space_until_final_comment, bind::bind);
-    let close_brace = pair(tokens::space, char('}'));
-    let binds = map(many_till(bind, close_brace), |(p, _)| Partial::from_iter(p));
-    let set = preceded(pair(char('{'), tokens::space_until_final_comment), binds);
-    map_partial(set, |binds| ExprSet::new(binds, input.to_byte_span()))(input)
+    let binds = preceded(char('{'), many_till(bind, pair(tokens::space, char('}'))));;
+    let set = map(binds, |(p, _)| Partial::from_iter(p));
+    map_partial_spanned(input, set, |span, binds| ExprSet::new(binds, span))(input)
 }

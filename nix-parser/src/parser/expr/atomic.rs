db49b46b5cb35caccbd44bd4791eb3fcc1bfe9d8
Eyal Kalderon <ebkalderon@gmail.com>
2019-08-21 03:24:03 +0800

    Add list() atomic parser

--- nix-parser/src/parser/expr/atomic.rs
+++ nix-parser/src/parser/expr/atomic.rs
@@ -1,13 +1,16 @@
 use std::iter::FromIterator;
 
-use nom::character::complete::char;
-use nom::combinator::map;
-use nom::multi::many_till;
+use nom::branch::alt;
+use nom::character::complete::{anychar, char};
+use nom::combinator::{map, opt, peek};
+use nom::error::ErrorKind;
+use nom::error::VerboseError;
+use nom::multi::{many0, many_till};
 use nom::sequence::{pair, preceded};
 
 use super::{bind, expr};
-use crate::ast::{ExprParen, ExprSet};
-use crate::parser::partial::{expect_terminated, map_partial_spanned, Partial};
+use crate::ast::{ExprList, ExprParen, ExprSet};
+use crate::parser::partial::{expect_terminated, map_err_partial, map_partial_spanned, Partial};
 use crate::parser::{tokens, IResult, Span};
 
 pub fn paren(input: Span) -> IResult<Partial<ExprParen>> {
@@ -17,7 +20,14 @@ pub fn paren(input: Span) -> IResult<Partial<ExprParen>> {
 
 pub fn set(input: Span) -> IResult<Partial<ExprSet>> {
     let bind = preceded(tokens::space_until_final_comment, bind::bind);
-    let binds = preceded(char('{'), many_till(bind, pair(tokens::space, char('}'))));;
+    let binds = preceded(char('{'), many_till(bind, pair(tokens::space, char('}'))));
     let set = map(binds, |(p, _)| Partial::from_iter(p));
     map_partial_spanned(set, |span, binds| ExprSet::new(binds, span))(input)
+}
+
+pub fn list(input: Span) -> IResult<Partial<ExprList>> {
+    let elem = preceded(tokens::space, expr);
+    let elems = preceded(char('['), many_till(elem, char(']')));
+    let list = map(elems, |(p, _)| Partial::from_iter(p));
+    map_partial_spanned(list, |span, exprs| ExprList::new(exprs, span))(input)
 }

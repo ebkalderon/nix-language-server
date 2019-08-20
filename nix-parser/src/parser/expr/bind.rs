4409960bc28c9e6037c3a9185ed4901b9cd2a4e6
Eyal Kalderon <ebkalderon@gmail.com>
2019-08-20 21:03:57 +0800

    Update bind() to be partial, add invalid error case

--- nix-parser/src/parser/expr/bind.rs
+++ nix-parser/src/parser/expr/bind.rs
@@ -1,58 +1,61 @@
 use nom::branch::alt;
-use nom::character::complete::{char, multispace0};
-use nom::combinator::{cut, map, opt};
-use nom::multi::many1;
+use nom::character::complete::{anychar, char, multispace0};
+use nom::combinator::{cut, map, opt, recognize};
+use nom::error::{ErrorKind, VerboseError, VerboseErrorKind};
+use nom::multi::{many1, many_till};
 use nom::sequence::{delimited, pair, preceded, separated_pair, terminated};
 
 use super::expr;
 use crate::ast::{Bind, BindInherit, BindInheritExpr, BindSimple};
-use crate::parser::partial::verify_full;
+use crate::parser::partial::{map_partial, partial_or, Partial};
 use crate::parser::{tokens, IResult, Span};
 use crate::ToByteSpan;
 
-pub fn bind(input: Span) -> IResult<Bind> {
-    let inherit = map(inherit, Bind::Inherit);
-    let inherit_expr = map(inherit_expr, Bind::InheritExpr);
-    let simple = map(simple, Bind::Simple);
-    alt((inherit, inherit_expr, simple))(input)
+pub fn bind(input: Span) -> IResult<Partial<Bind>> {
+    let inherit = map_partial(inherit, Bind::Inherit);
+    let inherit_expr = map_partial(inherit_expr, Bind::InheritExpr);
+    let simple = map_partial(simple, Bind::Simple);
+    let bind = alt((inherit, inherit_expr, simple));
+    partial_or(bind, char(';'), ErrorKind::IsA)(input)
 }
 
-fn simple(input: Span) -> IResult<BindSimple> {
+fn simple(input: Span) -> IResult<Partial<BindSimple>> {
     let comment = opt(terminated(tokens::comment, multispace0));
 
     let lhs = terminated(tokens::ident_path, tokens::space);
-    let equals = pair(cut(char('=')), tokens::space);
-    let rhs = terminated(map(verify_full(expr), Box::new), cut(char(';')));
+    let equals = pair(char('='), tokens::space);
+    let rhs = terminated(map_partial(expr, Box::new), char(';'));
     let bind = pair(comment, separated_pair(lhs, equals, rhs));
 
     map(bind, |(comment, (name, expr))| {
-        BindSimple::new(comment, name, expr, input.to_byte_span())
+        expr.map(|expr| BindSimple::new(comment, name, expr, input.to_byte_span()))
     })(input)
 }
 
-fn inherit(input: Span) -> IResult<BindInherit> {
+fn inherit(input: Span) -> IResult<Partial<BindInherit>> {
     let comment = opt(terminated(tokens::comment, multispace0));
     let key_inherit = pair(tokens::keyword_inherit, tokens::space);
     let name = terminated(tokens::identifier, tokens::space);
-    let bind = preceded(comment, delimited(key_inherit, many1(name), cut(char(';'))));
-    map(bind, |names| BindInherit::new(names, input.to_byte_span()))(input)
+    let bind = preceded(comment, delimited(key_inherit, many1(name), char(';')));
+    map(bind, |names| {
+        Partial::from(BindInherit::new(names, input.to_byte_span()))
+    })(input)
 }
 
-fn inherit_expr(input: Span) -> IResult<BindInheritExpr> {
+fn inherit_expr(input: Span) -> IResult<Partial<BindInheritExpr>> {
     let comment = opt(terminated(tokens::comment, multispace0));
     let key_inherit = pair(tokens::keyword_inherit, tokens::space);
 
     let open_paren = pair(char('('), tokens::space);
     let close_paren = pair(cut(char(')')), tokens::space);
-    let verified = verify_full(expr);
-    let expr = map(delimited(open_paren, verified, close_paren), Box::new);
+    let expr = map_partial(delimited(open_paren, expr, close_paren), Box::new);
 
     let name = terminated(tokens::identifier, tokens::space);
-    let inherit = delimited(key_inherit, pair(expr, many1(name)), cut(char(';')));
+    let inherit = delimited(key_inherit, pair(expr, many1(name)), char(';'));
     let bind = preceded(comment, inherit);
 
     map(bind, |(expr, names)| {
-        BindInheritExpr::new(expr, names, input.to_byte_span())
+        expr.map(|expr| BindInheritExpr::new(expr, names, input.to_byte_span()))
     })(input)
 }
 
@@ -71,24 +74,24 @@ mod tests {
         let (_, uncommented) = all_consuming(simple)(string).unwrap();
         assert_eq!(
             uncommented,
-            BindSimple::new(
+            Partial::from(BindSimple::new(
                 None,
                 IdentPath::from(vec!["foo", "bar"]),
                 Box::new(Expr::Literal(Literal::from(true))),
                 Span::new("").to_byte_span(),
-            )
+            ))
         );
 
         let string = Span::new("# hello world \n #this is a   doc comment   \n  foo.bar = true;");
         let (_, commented) = all_consuming(simple)(string).unwrap();
         assert_eq!(
             commented,
-            BindSimple::new(
+            Partial::from(BindSimple::new(
                 Some(Comment::from(" hello world \nthis is a   doc comment   ")),
                 IdentPath::from(vec!["foo", "bar"]),
                 Box::new(Expr::Literal(Literal::from(true))),
                 Span::new("").to_byte_span(),
-            )
+            ))
         );
     }
 }

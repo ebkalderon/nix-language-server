188bd28914bea1464e260599a69f428af30eed88
Eyal Kalderon <ebkalderon@gmail.com>
2019-08-23 00:23:20 +0800

    Rewrite list() and set() to gracefully recover from missing delims

--- nix-parser/src/parser/expr/atomic.rs
+++ nix-parser/src/parser/expr/atomic.rs
@@ -10,7 +10,7 @@ use nom::sequence::{pair, preceded, terminated};
 
 use super::{bind, expr, unary};
 use crate::ast::{ExprList, ExprParen, ExprSet};
-use crate::parser::partial::{expect_terminated, map_err_partial, map_partial_spanned, Partial};
+use crate::parser::partial::{expect_terminated, many_till_partial, map_partial_spanned, Partial};
 use crate::parser::{tokens, IResult, Span};
 
 pub fn paren(input: Span) -> IResult<Partial<ExprParen>> {
@@ -20,14 +20,14 @@ pub fn paren(input: Span) -> IResult<Partial<ExprParen>> {
 
 pub fn set(input: Span) -> IResult<Partial<ExprSet>> {
     let bind = preceded(tokens::space_until_final_comment, bind::bind);
-    let binds = preceded(char('{'), many_till(bind, pair(tokens::space, char('}'))));
-    let set = map(binds, |(p, _)| Partial::from_iter(p));
+    let binds = many_till_partial(bind, peek(pair(tokens::space, char('}'))));
+    let set = expect_terminated(preceded(char('{'), binds), pair(tokens::space, char('}')));
     map_partial_spanned(set, |span, binds| ExprSet::new(binds, span))(input)
 }
 
 pub fn list(input: Span) -> IResult<Partial<ExprList>> {
     let elem = terminated(unary, tokens::space);
-    let elems = map(many_till(elem, char(']')), |(p, _)| Partial::from_iter(p));
-    let list = preceded(pair(char('['), tokens::space), elems);
+    let elems = many_till_partial(elem, char(']'));
+    let list = expect_terminated(preceded(pair(char('['), tokens::space), elems), char(']'));
     map_partial_spanned(list, |span, exprs| ExprList::new(exprs, span))(input)
 }

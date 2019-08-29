4c0e5a77b842ce3987d0e66835e0c937a9ba4185
Eyal Kalderon <ebkalderon@gmail.com>
2019-08-29 12:00:13 +0800

    Move server module into separate tower-lsp crate

---
+++ tower-lsp/src/lib.rs
@@ -0,0 +1,138 @@
+//! Language Server Protocol (LSP) server abstraction for Tower.
+//!
+//! # Example
+//!
+//! ```rust
+//! # use futures::future::{self, Future};
+//! # use lsp_types::*;
+//! # use jsonrpc_core::{Result, BoxFuture};
+//! # use tower_lsp::{Printer, LanguageServer, Server, LspService};
+//! #
+//! #[derive(Debug, Default)]
+//! struct Backend;
+//!
+//! impl LanguageServer for Backend {
+//!     type ShutdownFuture = BoxFuture<()>;
+//!     type HighlightFuture = BoxFuture<Option<Vec<DocumentHighlight>>>;
+//!     type HoverFuture = BoxFuture<Option<Hover>>;
+//!
+//!     fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+//!         Ok(InitializeResult::default())
+//!     }
+//!
+//!     fn initialized(&self, printer: &Printer, _: InitializedParams) {
+//!         printer.log_message(MessageType::Info, "server initialized!");
+//!     }
+//!
+//!     fn shutdown(&self) -> Self::ShutdownFuture {
+//!         Box::new(future::ok(()))
+//!     }
+//!
+//!     fn did_open(&self, printer: &Printer, _: DidOpenTextDocumentParams) {
+//!         printer.log_message(MessageType::Info, "file opened!");
+//!     }
+//!
+//!     fn did_save(&self, printer: &Printer, _: DidSaveTextDocumentParams) {
+//!         printer.log_message(MessageType::Info, "file saved!");
+//!     }
+//!
+//!     fn did_change(&self, printer: &Printer, _: DidChangeTextDocumentParams) {
+//!         printer.log_message(MessageType::Info, "file changed!");
+//!     }
+//!
+//!     fn highlight(&self, _: TextDocumentPositionParams) -> Self::HighlightFuture {
+//!         Box::new(future::ok(None))
+//!     }
+//!
+//!     fn hover(&self, _: TextDocumentPositionParams) -> Self::HoverFuture {
+//!         Box::new(future::ok(None))
+//!     }
+//! }
+//!
+//! fn main() {
+//!     let stdin = tokio::io::stdin();
+//!     let stdout = tokio::io::stdout();
+//!
+//!     let (service, messages) = LspService::new(Backend::default());
+//!     let handle = service.close_handle();
+//!     let server = Server::new(stdin, stdout)
+//!         .interleave(messages)
+//!         .serve(service);
+//!
+//!     tokio::run(handle.run_until_exit(server));
+//! }
+//! ```
+
+pub extern crate lsp_types;
+
+pub use self::service::{ExitReceiver, LspService, MessageStream, Printer};
+pub use self::stdio::Server;
+
+use futures::Future;
+use jsonrpc_core::{Error, Result};
+use lsp_types::*;
+
+mod codec;
+mod delegate;
+mod service;
+mod stdio;
+
+pub trait LanguageServer: Send + Sync + 'static {
+    type ShutdownFuture: Future<Item = (), Error = Error> + Send;
+    type HighlightFuture: Future<Item = Option<Vec<DocumentHighlight>>, Error = Error> + Send;
+    type HoverFuture: Future<Item = Option<Hover>, Error = Error> + Send;
+
+    fn initialize(&self, params: InitializeParams) -> Result<InitializeResult>;
+
+    fn initialized(&self, printer: &Printer, params: InitializedParams);
+
+    fn shutdown(&self) -> Self::ShutdownFuture;
+
+    fn did_open(&self, printer: &Printer, params: DidOpenTextDocumentParams);
+
+    fn did_save(&self, printer: &Printer, params: DidSaveTextDocumentParams);
+
+    fn did_change(&self, printer: &Printer, params: DidChangeTextDocumentParams);
+
+    fn highlight(&self, params: TextDocumentPositionParams) -> Self::HighlightFuture;
+
+    fn hover(&self, params: TextDocumentPositionParams) -> Self::HoverFuture;
+}
+
+impl<S: ?Sized + LanguageServer> LanguageServer for Box<S> {
+    type ShutdownFuture = S::ShutdownFuture;
+    type HighlightFuture = S::HighlightFuture;
+    type HoverFuture = S::HoverFuture;
+
+    fn initialize(&self, params: InitializeParams) -> Result<InitializeResult> {
+        (**self).initialize(params)
+    }
+
+    fn initialized(&self, printer: &Printer, params: InitializedParams) {
+        (**self).initialized(printer, params);
+    }
+
+    fn shutdown(&self) -> Self::ShutdownFuture {
+        (**self).shutdown()
+    }
+
+    fn did_open(&self, printer: &Printer, params: DidOpenTextDocumentParams) {
+        (**self).did_open(printer, params);
+    }
+
+    fn did_save(&self, printer: &Printer, params: DidSaveTextDocumentParams) {
+        (**self).did_save(printer, params);
+    }
+
+    fn did_change(&self, printer: &Printer, params: DidChangeTextDocumentParams) {
+        (**self).did_change(printer, params);
+    }
+
+    fn highlight(&self, params: TextDocumentPositionParams) -> Self::HighlightFuture {
+        (**self).highlight(params)
+    }
+
+    fn hover(&self, params: TextDocumentPositionParams) -> Self::HoverFuture {
+        (**self).hover(params)
+    }
+}
---
+++ tower-lsp/src/service.rs
@@ -0,0 +1,182 @@
+use std::fmt::Display;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+
+use futures::future::{self, Future, Shared, SharedError, SharedItem};
+use futures::sink::Sink;
+use futures::stream::Stream;
+use futures::sync::mpsc;
+use futures::sync::oneshot::{self, Canceled};
+use futures::{Async, Poll};
+use jsonrpc_core::IoHandler;
+use log::{error, info, trace};
+use lsp_types::*;
+use serde::Serialize;
+use tower::Service;
+
+use super::delegate::{Delegate, LanguageServerCore};
+use super::LanguageServer;
+
+#[derive(Clone, Debug)]
+pub struct ExitReceiver(Shared<oneshot::Receiver<()>>);
+
+impl ExitReceiver {
+    pub fn run_until_exit<F>(self, future: F) -> impl Future<Item = (), Error = ()> + Send + 'static
+    where
+        F: Future<Item = (), Error = ()> + Send + 'static,
+    {
+        self.0.then(|_| Ok(())).select(future).then(|_| Ok(()))
+    }
+}
+
+impl Future for ExitReceiver {
+    type Item = SharedItem<()>;
+    type Error = SharedError<Canceled>;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        self.0.poll()
+    }
+}
+
+#[derive(Debug)]
+pub struct MessageStream(mpsc::Receiver<String>);
+
+impl Stream for MessageStream {
+    type Item = String;
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<Option<String>, ()> {
+        self.0.poll()
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Printer(mpsc::Sender<String>);
+
+impl Printer {
+    pub fn log_message<M: Display>(&self, typ: MessageType, message: M) {
+        self.send_notification(
+            "window/logMessage",
+            LogMessageParams {
+                typ,
+                message: message.to_string(),
+            },
+        );
+    }
+
+    pub fn publish_diagnostics(&self, uri: Url, diagnostics: Vec<Diagnostic>) {
+        let params = PublishDiagnosticsParams::new(uri, diagnostics);
+        self.send_notification("textDocument/publishDiagnostics", params);
+    }
+
+    pub fn show_message<M: Display>(&self, typ: MessageType, message: M) {
+        self.send_notification(
+            "window/showMessage",
+            ShowMessageParams {
+                typ,
+                message: message.to_string(),
+            },
+        );
+    }
+
+    fn send_notification<S: Serialize>(&self, method: &str, params: S) {
+        match serde_json::to_string(&params) {
+            Err(err) => error!("failed to serialize message for `{}`: {}", method, err),
+            Ok(params) => {
+                let message = format!(
+                    r#"{{"jsonrpc":"2.0","method":"{}","params":{}}}"#,
+                    method, params
+                );
+                tokio::spawn(
+                    self.0
+                        .clone()
+                        .send(message)
+                        .map(|_| ())
+                        .map_err(|_| error!("failed to send message")),
+                );
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct LspService {
+    handler: IoHandler,
+    exit_rx: ExitReceiver,
+    stopped: Arc<AtomicBool>,
+}
+
+impl LspService {
+    pub fn new<T>(server: T) -> (Self, MessageStream)
+    where
+        T: LanguageServer,
+    {
+        Self::with_handler(server, IoHandler::new())
+    }
+
+    pub fn with_handler<T, U>(server: T, handler: U) -> (Self, MessageStream)
+    where
+        T: LanguageServer,
+        U: Into<IoHandler>,
+    {
+        let (tx, rx) = mpsc::channel(1);
+        let print_tx = Printer(tx);
+        let print_rx = MessageStream(rx);
+
+        let mut handler = handler.into();
+        handler.extend_with(Delegate::new(server, print_tx).to_delegate());
+
+        let (tx, rx) = oneshot::channel();
+        let exit_tx = Mutex::new(Some(tx));
+        let exit_rx = ExitReceiver(rx.shared());
+
+        let stopped = Arc::new(AtomicBool::new(false));
+        let stopped_arc = stopped.clone();
+        handler.add_notification("exit", move |_| {
+            if let Some(tx) = exit_tx.lock().unwrap_or_else(|tx| tx.into_inner()).take() {
+                info!("exit notification received, shutting down");
+                stopped_arc.store(true, Ordering::SeqCst);
+                tx.send(()).unwrap();
+            }
+        });
+
+        let service = LspService {
+            handler,
+            exit_rx,
+            stopped,
+        };
+
+        (service, print_rx)
+    }
+
+    pub fn close_handle(&self) -> ExitReceiver {
+        self.exit_rx.clone()
+    }
+}
+
+impl Service<String> for LspService {
+    type Response = String;
+    type Error = ();
+    type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error> + Send>;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        if self.stopped.load(Ordering::SeqCst) {
+            Ok(Async::NotReady)
+        } else {
+            Ok(Async::Ready(()))
+        }
+    }
+
+    fn call(&mut self, request: String) -> Self::Future {
+        if self.stopped.load(Ordering::SeqCst) {
+            Box::new(future::err(()))
+        } else {
+            Box::new(self.handler.handle_request(&request).map(move |result| {
+                result.unwrap_or_else(|| {
+                    trace!("request produced no response: {}", request);
+                    String::new()
+                })
+            }))
+        }
+    }
+}

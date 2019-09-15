f2bddd0a2e7baca958f263a553f456eb876dec9f
Eyal Kalderon <ebkalderon@gmail.com>
2019-09-15 18:14:30 +0800

    Add map_err() parser combinator

--- nix-parser/src/parser.rs
+++ nix-parser/src/parser.rs
@@ -4,10 +4,11 @@ use std::str::FromStr;
 
 use codespan::Span;
 use nom::combinator::{all_consuming, map, opt};
+use nom::error::ErrorKind;
 use nom::sequence::{pair, preceded};
 use nom::Slice;
 
-use self::error::Errors;
+use self::error::{Error, Errors};
 use self::partial::map_partial;
 use crate::ast::{Expr, SourceFile};
 use crate::ToSpan;
@@ -86,5 +87,34 @@ where
         let value_len = remainder.offset - input.offset;
         let span = input.slice(..value_len).to_span();
         Ok((remainder, f(span, value)))
+    }
+}
+
+/// Combinator which converts an underlying `nom` error, if any, into a custom parse error.
+pub fn map_err<'a, O, P, F, E>(parser: P, op: F) -> impl Fn(LocatedSpan<'a>) -> IResult<O>
+where
+    P: Fn(LocatedSpan<'a>) -> IResult<O>,
+    F: Fn(Span, &str, ErrorKind) -> E,
+    E: Into<Error>,
+{
+    move |input| match parser(input) {
+        Ok(output) => Ok(output),
+        Err(nom::Err::Error(mut err)) => {
+            if let Some((span, frag, kind)) = err.last().and_then(|e| e.as_nom_error()) {
+                let new_err = op(span, frag, kind);
+                err.pop();
+                err.push(new_err);
+            }
+            Err(nom::Err::Error(err))
+        }
+        Err(nom::Err::Failure(mut err)) => {
+            if let Some((span, frag, kind)) = err.last().and_then(|e| e.as_nom_error()) {
+                let new_err = op(span, frag, kind);
+                err.pop();
+                err.push(new_err);
+            }
+            Err(nom::Err::Failure(err))
+        }
+        Err(err) => Err(err),
     }
 }

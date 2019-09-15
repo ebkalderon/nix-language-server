70bd6a367c639eb54744f5f44067c0cd04e9d3ef
Eyal Kalderon <ebkalderon@gmail.com>
2019-09-15 18:34:02 +0800

    Use map_err() in keyword parser combinators

--- nix-parser/src/parser/tokens/keywords.rs
+++ nix-parser/src/parser/tokens/keywords.rs
@@ -4,7 +4,8 @@ use nom::character::complete::alphanumeric1;
 use nom::combinator::{not, peek};
 use nom::sequence::terminated;
 
-use crate::parser::{IResult, LocatedSpan};
+use crate::parser::error::ExpectedFoundError;
+use crate::parser::{map_err, IResult, LocatedSpan};
 
 macro_rules! define_keywords {
     ($($function:ident => $keyword:tt),+) => {
@@ -22,7 +23,19 @@ macro_rules! define_keywords {
 
         $(
             pub fn $function(input: LocatedSpan) -> IResult<LocatedSpan> {
-                tag(stringify!($keyword))(input)
+                map_err(tag(stringify!($keyword)), |span, frag, _| {
+                    let found = if frag.chars().next().filter(|c| c.is_alphabetic()).is_some() {
+                        frag.split_whitespace().next().map(|s| format!("`{}`", s)).unwrap()
+                    } else {
+                        frag.chars().next().map(|c| format!("`{}`", c)).unwrap_or_else(|| "EOF".into())
+                    };
+
+                    ExpectedFoundError::new(
+                        vec![format!("keyword `{}`", stringify!($keyword))],
+                        found,
+                        span,
+                    )
+                })(input)
             }
         )+
     };

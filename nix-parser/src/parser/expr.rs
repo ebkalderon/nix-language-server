781b5e03bdf104521865f51dcc48721ed2710f9f
Eyal Kalderon <ebkalderon@gmail.com>
2019-09-21 00:44:51 +0800

    Add support for comparison, update, sum, product, and unary ops

--- nix-parser/src/parser/expr.rs
+++ nix-parser/src/parser/expr.rs
@@ -1,10 +1,11 @@
 use codespan::Span;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{char, space1};
+use nom::character::complete::char;
 use nom::combinator::{map, opt};
 use nom::multi::many0;
 use nom::sequence::{delimited, pair, preceded, terminated, tuple};
+use std::iter::{self, FromIterator};
 
 use super::partial::{map_partial, Partial};
 use super::{map_spanned, tokens, IResult, LocatedSpan};
@@ -71,7 +72,7 @@ fn equality(input: LocatedSpan) -> IResult<Partial<Expr>> {
     let neq = map(tag("!="), |_| BinaryOp::NotEq);
 
     let op = delimited(tokens::space, alt((eq, neq)), tokens::space);
-    let expr = pair(unary, opt(pair(op, unary)));
+    let expr = pair(compare, opt(pair(op, compare)));
     map(expr, |(lhs, op)| match op {
         None => lhs,
         Some((op, rhs)) => lhs.flat_map(|lhs| {
@@ -81,6 +82,94 @@ fn equality(input: LocatedSpan) -> IResult<Partial<Expr>> {
                 Expr::Binary(expr)
             })
         }),
+    })(input)
+}
+
+fn compare(input: LocatedSpan) -> IResult<Partial<Expr>> {
+    let lte = map(tag("<="), |_| BinaryOp::LessThanEq);
+    let lt = map(char('<'), |_| BinaryOp::LessThan);
+    let gte = map(tag(">="), |_| BinaryOp::LessThanEq);
+    let gt = map(char('>'), |_| BinaryOp::LessThan);
+
+    let op = delimited(tokens::space, alt((lte, lt, gte, gt)), tokens::space);
+    let expr = pair(update, opt(pair(op, update)));
+    map(expr, |(lhs, op)| match op {
+        None => lhs,
+        Some((op, rhs)) => lhs.flat_map(|lhs| {
+            rhs.map(|rhs| {
+                let span = Span::merge(lhs.span(), rhs.span());
+                let expr = ExprBinary::new(op, Box::new(lhs), Box::new(rhs), span);
+                Expr::Binary(expr)
+            })
+        }),
+    })(input)
+}
+
+fn update(input: LocatedSpan) -> IResult<Partial<Expr>> {
+    let op = delimited(tokens::space, tag("//"), tokens::space);
+    let expr = pair(sum, many0(preceded(op, sum)));
+    map(expr, |(first, rest)| {
+        let exprs = Partial::from_iter(iter::once(first).chain(rest));
+        exprs.map(|mut exprs| {
+            let last = exprs.pop().unwrap();
+            exprs.into_iter().rev().fold(last, |lhs, rhs| {
+                let span = Span::merge(rhs.span(), lhs.span());
+                let expr = ExprBinary::new(BinaryOp::Update, Box::new(rhs), Box::new(lhs), span);
+                Expr::Binary(expr)
+            })
+        })
+    })(input)
+}
+
+fn sum(input: LocatedSpan) -> IResult<Partial<Expr>> {
+    let add = map(char('+'), |_| BinaryOp::Add);
+    let sub = map(char('-'), |_| BinaryOp::Sub);
+    let op = delimited(tokens::space, alt((add, sub)), tokens::space);
+    let expr = pair(product, many0(pair(op, product)));
+    map(expr, |(first, rest)| {
+        rest.into_iter().fold(first, |lhs, (op, rhs)| {
+            lhs.flat_map(|lhs| {
+                rhs.map(|rhs| {
+                    let span = Span::merge(lhs.span(), rhs.span());
+                    let expr = ExprBinary::new(op, Box::new(lhs), Box::new(rhs), span);
+                    Expr::Binary(expr)
+                })
+            })
+        })
+    })(input)
+}
+
+fn product(input: LocatedSpan) -> IResult<Partial<Expr>> {
+    let mul = map(char('*'), |_| BinaryOp::Mul);
+    let div = map(char('/'), |_| BinaryOp::Div);
+    let op = delimited(tokens::space, alt((mul, div)), tokens::space);
+    let expr = pair(concat, many0(pair(op, concat)));
+    map(expr, |(first, rest)| {
+        rest.into_iter().fold(first, |lhs, (op, rhs)| {
+            lhs.flat_map(|lhs| {
+                rhs.map(|rhs| {
+                    let span = Span::merge(lhs.span(), rhs.span());
+                    let expr = ExprBinary::new(op, Box::new(lhs), Box::new(rhs), span);
+                    Expr::Binary(expr)
+                })
+            })
+        })
+    })(input)
+}
+
+fn concat(input: LocatedSpan) -> IResult<Partial<Expr>> {
+    let op = delimited(tokens::space, tag("++"), tokens::space);
+    let expr = pair(unary, many0(preceded(op, unary)));
+    map(expr, |(first, rest)| {
+        let exprs = Partial::from_iter(iter::once(first).chain(rest));
+        exprs.map(|mut exprs| {
+            let last = exprs.pop().unwrap();
+            exprs.into_iter().rev().fold(last, |lhs, rhs| {
+                let span = Span::merge(rhs.span(), lhs.span());
+                let expr = ExprBinary::new(BinaryOp::Concat, Box::new(rhs), Box::new(lhs), span);
+                Expr::Binary(expr)
+            })
+        })
     })(input)
 }
 

b79fc7a47a022cd42131a44258237754080fa6a4
Eyal Kalderon <ebkalderon@gmail.com>
2019-09-27 15:57:21 +0800

    Add support for sets, rec sets, legacy let

--- nix-parser/src/parser/expr.rs
+++ nix-parser/src/parser/expr.rs
@@ -47,9 +47,11 @@ fn fn_app(input: Tokens) -> IResult<Partial<Expr>> {
 fn atomic(input: Tokens) -> IResult<Partial<Expr>> {
     let paren = map_partial(atomic::paren, Expr::Paren);
     let set = map_partial(atomic::set, Expr::Set);
+    let rec_set = map_partial(atomic::rec_set, Expr::Rec);
+    let let_set = map_partial(atomic::let_set, Expr::Let);
     let list = map_partial(atomic::list, Expr::List);
     let literal = map_partial(atomic::literal, Expr::Literal);
-    alt((paren, set, list, literal))(input)
+    alt((paren, set, rec_set, let_set, list, literal))(input)
 }
 
 fn unknown(input: Tokens) -> IResult<Partial<Expr>> {
--- nix-parser/src/parser/expr/atomic.rs
+++ nix-parser/src/parser/expr/atomic.rs
@@ -2,9 +2,9 @@ use nom::branch::alt;
 use nom::combinator::map;
 use nom::sequence::preceded;
 
-use super::{expr, unary};
+use super::{bind, expr, unary};
 use crate::ast::tokens::Literal;
-use crate::ast::{ExprList, ExprParen};
+use crate::ast::{Bind, ExprLet, ExprList, ExprParen, ExprRec, ExprSet};
 use crate::lexer::Tokens;
 use crate::parser::partial::{expect_terminated, many_till_partial, map_partial_spanned, Partial};
 use crate::parser::{tokens, IResult};
@@ -12,6 +12,20 @@ use crate::parser::{tokens, IResult};
 pub fn paren(input: Tokens) -> IResult<Partial<ExprParen>> {
     let paren = expect_terminated(preceded(tokens::paren_left, expr), tokens::paren_right);
     map_partial_spanned(paren, |span, inner| ExprParen::new(Box::new(inner), span))(input)
+}
+
+pub fn set(input: Tokens) -> IResult<Partial<ExprSet>> {
+    map_partial_spanned(set_binds, |span, binds| ExprSet::new(binds, span))(input)
+}
+
+pub fn rec_set(input: Tokens) -> IResult<Partial<ExprRec>> {
+    let rec_set = preceded(tokens::keyword_rec, set_binds);
+    map_partial_spanned(rec_set, |span, binds| ExprRec::new(binds, span))(input)
+}
+
+pub fn let_set(input: Tokens) -> IResult<Partial<ExprLet>> {
+    let let_set = preceded(tokens::keyword_let, set_binds);
+    map_partial_spanned(let_set, |span, binds| ExprLet::new(binds, span))(input)
 }
 
 pub fn list(input: Tokens) -> IResult<Partial<ExprList>> {
@@ -29,4 +43,9 @@ pub fn literal(input: Tokens) -> IResult<Partial<Literal>> {
     let path_template = map(tokens::path_template, Partial::from);
     let uri = map(tokens::uri, Partial::from);
     alt((boolean, null, float, integer, path, path_template, uri))(input)
+}
+
+fn set_binds(input: Tokens) -> IResult<Partial<Vec<Bind>>> {
+    let binds = many_till_partial(bind::bind, alt((tokens::brace_right, tokens::semi)));
+    expect_terminated(preceded(tokens::brace_left, binds), tokens::brace_right)(input)
 }

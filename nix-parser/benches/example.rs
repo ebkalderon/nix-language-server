5bd123b735533c52e19bbba90cde96f9fa878208
Eyal Kalderon <ebkalderon@gmail.com>
2019-10-14 18:35:37 +0800

    Optimize example.rs bench test
    This commit switches `.iter()` with `.iter_with_large_drop()` to avoid
    timing the destructor of the large `SourceFile` object, which makes
    readings more consistent. To make reading the results a bit easier, a
    throughput measurement has been added to the benchmark as well.
    
    ```
    parse_example/parse example.nix
                            time:   [660.11 us 661.79 us 663.78 us]
                            thrpt:  [737.08 KiB/s 739.29 KiB/s 741.18 KiB/s]
                     change:
                            time:   [-5.0056% -1.0921% +3.3765%] (p = 0.61 > 0.05)
                            thrpt:  [-3.2662% +1.1041% +5.2694%]
                            No change in performance detected.
    Found 10 outliers among 100 measurements (10.00%)
      4 (4.00%) high mild
      6 (6.00%) high severe
    ```

--- nix-parser/benches/example.rs
+++ nix-parser/benches/example.rs
@@ -1,15 +1,22 @@
 use std::str::FromStr;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use nix_parser::ast::SourceFile;
 
 const EXAMPLE_FILE: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/example.nix"));
 
 fn parse_example(b: &mut Criterion) {
+    let mut group = b.benchmark_group("parse_example");
+
     let module = black_box(EXAMPLE_FILE);
-    b.bench_function("parse example.nix", move |b| {
-        b.iter(|| SourceFile::from_str(module).expect("Failed to parse example.nix"));
+    group.throughput(Throughput::Bytes(EXAMPLE_FILE.len() as u64));
+    group.bench_function("parse example.nix", move |b| {
+        b.iter_with_large_drop(|| {
+            SourceFile::from_str(module).expect("Failed to parse example.nix")
+        });
     });
+
+    group.finish()
 }
 
 criterion_group!(benches, parse_example);

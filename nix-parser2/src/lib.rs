6cf8b7f2e74213abe97173f31d62f163d85c9d57
Eyal Kalderon <ebkalderon@gmail.com>
2020-03-23 02:16:59 +0800

    Document rationale behind ToSpan trait

--- nix-parser2/src/lib.rs
+++ nix-parser2/src/lib.rs
@@ -3,7 +3,11 @@ use codespan::Span;
 pub mod error;
 pub mod lexer;
 
+/// A trait for converting a value to a `codespan::Span`.
+///
+/// This is helpful for getting spanned types from external crates to interoperate with `codespan`.
 pub trait ToSpan {
+    /// Converts the given value to a `Span`.
     fn to_span(&self) -> Span;
 }
 
